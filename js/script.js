273b449e4868ceeb5eb1d6aae886fb8a9012acf1
Guille-Rubio <guillermorubiog@gmail.com>
2022-02-17 23:16:58 +0100

    Js pinta respuestas, pendiente arreglar event listeners y area de clic respuesta

--- js/script.js
+++ js/script.js
@@ -1,27 +1,116 @@
 //pasar de la portada a la primera pregunta
+// Home > p1   
+
+//ARRAY con OBJETOs Pregunta
+
+const preguntas = [
+    {
+        pregunta: "Pregunta1",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correcta: "a"
+    },
+    {
+        pregunta: "Pregunta2",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correcta: "a"
+    },
+    {
+        pregunta: "Pregunta3",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correcta: "a"
+    },
+    {
+        pregunta: "Pregunta4",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correcta: "a"
+    },
+    {
+        pregunta: "Pregunta5",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correcta: "a"
+    }
+]
+
+//Home > p1 >p2 >p3> p4> p5 > hoja respuestas con submit
+
+//FUNCION
+
+const pregunta = document.querySelector("h3");
+const opcion1 = document.querySelectorAll("label")[0];
+const opcion2 = document.querySelectorAll("label")[1];
+const opcion3 = document.querySelectorAll("label")[2];
+const opcion4 = document.querySelectorAll("label")[3];
+
+let i = 0;
+
+const fichas = document.getElementsByClassName("ficha");
+
+window.onload = cargarPrimeraVez();
+
+function cargarPregunta() {
+    if(i>4){
+        return
+    }
+    pregunta.innerHTML = preguntas[i].pregunta;
+    opcion1.innerHTML = preguntas[i].a;
+    opcion2.innerHTML = preguntas[i].b;
+    opcion3.innerHTML = preguntas[i].b;
+    opcion4.innerHTML = preguntas[i].d;
+    
+    i++;
+}
+
+//arreglar event listener de fichas a label !!!!
+
+function cargarPrimeraVez() {
+    fichas[0].addEventListener("click", cargarPregunta, true);
+    fichas[1].addEventListener("click", cargarPregunta, true);
+    fichas[2].addEventListener("click", cargarPregunta, true);
+    fichas[3].addEventListener("click", cargarPregunta, true);
+    i=0;
+    pregunta.innerHTML = preguntas[i].pregunta;
+    opcion1.innerHTML = preguntas[i].a;
+    opcion2.innerHTML = preguntas[i].b;
+    opcion3.innerHTML = preguntas[i].b;
+    opcion4.innerHTML = preguntas[i].d;
+}
 
 
 
 
+
+
+/*
+- pintar la nueva pregunta y las cuatro respuestas
+- que guarde la respuesta
+*/
+
+
+
+
+
+
+
+//Tabla de respuestas
 
 
 
 //Al pulsar un boton pase a la siguiente pregunta (Sin terminar)
-let ficha = document.getElementsByClassName("ficha");
-let pregunta = document.getElementsByTagName("fieldset");
-let preguntaActiva = 0;
-
-
-
-function mostrarSiguiente(event){
-    event.target.classList.toggle("escondido");
-    event.target[preguntaActiva+1].classList.toggle("escondido");
-    preguntaActiva++;
-
-}
-
-ficha.addEventListener("click", mostrarSiguiente());
-
 
 
 
@@ -36,6 +125,9 @@ ficha.addEventListener("click", mostrarSiguiente());
 
 
 //crear objeto respuestas
+
+
+//crear botón de envío <button class="escondido" type="submit">Comprobar datos</button>
 
 
 
