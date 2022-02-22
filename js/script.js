1878336776ab4b3b1ed52f6d6db28ed2a2247b61
Guille-Rubio <guillermorubiog@gmail.com>
2022-02-22 17:28:56 +0100

    Refactorizacion, todas las respuestas se vuelven rojas

--- js/script.js
+++ js/script.js
@@ -5,16 +5,37 @@ document.querySelector("form").addEventListener('submit', function (event) {
 
     const numPreguntas = document.getElementsByTagName("fieldset").length;
 
-    for (let i = 0; i < numPreguntas * 4; i ++) {
+    for (let i = 0; i < numPreguntas * 4; i++) {
         if (document.getElementsByTagName("input")[i].checked === true || document.getElementsByTagName("input")[i].value === "correcto") {
             document.querySelectorAll(".ficha")[i].style.backgroundColor = "green";
         } else if (document.getElementsByTagName("input")[i].checked === true || document.getElementsByTagName("input")[i].value === "incorrecto") {
             document.querySelectorAll(".ficha")[i].style.backgroundColor = "red";
         }
     }
+
+//document.getElementsByTagName("input")[1].checked
+
+    const filtered = document.getElementsByTagName("input[type='radio']").filter(element => element.checked);
+
+
+    if (filtered == numPreguntas) {
+    } else { console.log("Deben responderse todas las preguntas") }
+
+    /* for (let j = 0; j < inputs.length - 1; i++) {
+        let k=0;
+        if(inputs[i].checked == true){
+            k++;
+        }
+    }
+    if(k<fieldset.lenght){
+        
+    } */
+
+
+
     window.scroll({
-        top: 0, 
-        left: 0, 
+        top: 0,
+        left: 0,
         behavior: 'smooth'
-      });
+    });
 });
